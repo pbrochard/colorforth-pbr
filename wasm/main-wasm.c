ad1628b38c501f21432e78eef52f47d8c22071f7
Philippe Brochard <hocwp@free.fr>
2024-04-15 14:33:44 +0200

    Minor wasm tweak

--- wasm/main-wasm.c
+++ wasm/main-wasm.c
@@ -18,7 +18,7 @@ main(int argc, char *argv[])
   s = colorforth_newstate();
   cf_printf(s, "This is a colorForth inspired implementation\n");
   cf_printf(s, "   source code: https://github.com/pbrochard/colorforth-pbr\n");
-  cf_printf(s, "Try for example: 2 2 + .\n");
-  cf_printf(s, "Type `words` the see defined words:\n");
+  cf_printf(s, "Try for example: `2 2 + .`\n");
+  cf_printf(s, "Type `words` the see the defined words\n");
   cf_printf(s, "~\n");
 }
