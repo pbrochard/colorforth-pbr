81a869ce9517ae21686f1c119dffb86e1078f36e
Philippe Brochard <hocwp@free.fr>
2024-04-26 21:45:27 +0200

    Tweak colors

--- src/color.h
+++ src/color.h
@@ -2,6 +2,8 @@
 #ifndef __ECHO_COLOR_H
 #define __ECHO_COLOR_H
 
+#ifdef __ECHO_COLOR
+
 #define COLOR_RED      "\x1B[01;91m"
 #define COLOR_MAGENTA  "\x1B[01;95m"
 #define COLOR_GREEN    "\x1B[01;92m"
@@ -11,15 +13,25 @@
 #define COLOR_WHITE    "\x1B[01;37m"
 #define COLOR_CLEAR    "\x1B[0m"
 
-#ifdef __ECHO_COLOR
-
 extern void echo_color(struct state *state, int c, char *color);
 extern void init_terminal(void);
 extern void reset_terminal(void);
+
 #else
+
+#define COLOR_RED      ""
+#define COLOR_MAGENTA  ""
+#define COLOR_GREEN    ""
+#define COLOR_YELLOW   ""
+#define COLOR_BLUE     ""
+#define COLOR_CYAN     ""
+#define COLOR_WHITE    ""
+#define COLOR_CLEAR    ""
+
 #define echo_color(state, c, color)
 #define init_terminal()
 #define reset_terminal()
+
 #endif
 
 #endif /* __ECHO_COLOR_H */
